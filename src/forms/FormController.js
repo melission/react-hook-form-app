a8747ee43d4e3b825ea0faf041ef5b037490a58b
Polina <polina@blsv.space>
2023-07-31 11:37:24 +0200

    a few form fields were added; "forms.css" was added as import.

--- src/forms/FormController.js
+++ src/forms/FormController.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./forms.css";
 import { useForm } from 'react-hook-form';
 
 const FormComponent = () => {
@@ -21,18 +22,33 @@ const FormComponent = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
+    <form className='form-block' onSubmit={handleSubmit(onSubmit)}>
+      <div className="form-element">
         <label>Name:</label>
-        <input {...register('name', { required: 'Name is required' })} />
+        <input {...register('name', { required: 'Name is required', pattern: /^[A-Za-z]+$/i })} />
         {errors.name && <span>{errors.name.message}</span>}
       </div>
-      <div>
+      <div className='form-element'>
         <label>Email:</label>
         <input {...register('email', { required: 'Email is required', pattern: { value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, message: 'Invalid email address' } })} />
         {errors.email && <span>{errors.email.message}</span>}
       </div>
-      <button type="submit">Submit</button>
+      <div className='form-element'>
+        <label>Gender:</label>
+        <input {...register("gender", {required: "Gender is required"})}>
+        <select>
+            <option value="she/her">she/her</option>
+            <option value="he/him">he/him</option>
+            <option value="them/they">them/they</option>
+            <option value="other">other</option>
+        </select>
+        </input>
+      </div>
+      <div className='form-element'>
+        <label>Age:</label>
+        <input type="number" {...register("age", {min: 18, max: 99})} />
+      </div>
+      <button className='form-button' type="submit">Submit</button>
     </form>
   );
 };
